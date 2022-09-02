2320d70599cbd6dd22fc6bdb74b43b661a483057
HRLO77 <shakebmohammad.10@gmail.com>
2022-09-02 17:19:35 -0400

    added weights file

--- dnn_classifier.py
+++ dnn_classifier.py
@@ -2,9 +2,8 @@ print('Importing libraries...')
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import pickle as pk
-from keras.preprocessing import sequence
 import keras
+
 
 print('Loading data...')
 words = [word.strip() for word in open('./wordle.txt').readlines()]
@@ -15,7 +14,7 @@ print('Indexing and labelling...')
 data = pd.read_csv('./wordle.csv')
 labels = np.array([int(i) for i in data.pop('labels').to_numpy(dtype=np.int16)])
 test =pd.read_csv('./test.csv')
-test_labels = np.array([int(i) for i in test.pop('labels').to_numpy(dtype=np.int8)])
+test_labels = np.array([int(i) for i in test.pop('labels').to_numpy(dtype=np.int16)])
 test.head(), data.head()
 
 data = data.to_numpy().flatten()
@@ -26,52 +25,42 @@ char2idx = {v:i for i,v in enumerate(t)}
 idx2char = {i:v for i,v in enumerate(t)}
 del t
 
-print('Test:')
-print(data[0], labels[0])
-print(idx_dict[labels[0]])
-
 def encode(array: np.ndarray):
     l = []
     for i in array:
         l.append([char2idx[v] for v in i])
-    return np.array(l, dtype=np.int8)
+    d = np.array(l, dtype=np.int8)
+    return d / max(char2idx.values())
 
 data = encode(data)
 test = encode(test)
 
-data = data / data.max()
-
-test = test / test.max()
-
 model = keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(5,)),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(len(set(labels)), activation='softmax')
-    ])
+  tf.keras.layers.Flatten(input_shape=(5,)),
+  tf.keras.layers.Dense(64*4*4, activation='relu'),
+  tf.keras.layers.Dense(64*4, activation='relu'),
+  tf.keras.layers.Dense(64, activation='relu'),
+  tf.keras.layers.Dense(len(set(labels)), activation='softmax')
+  ])
 
 model.compile(optimizer='nadam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
 model.summary()
 
+model.load_weights('./model.h5') 
 
-model.fit(data, labels, epochs=3)
+model.fit(data, labels, epochs=7)
 
-model.save('./model.h5')
-
+model.save('./model1.h5') 
 history = model.evaluate(test, test_labels)
-
-print(history)
-
 
 def predict(arr: np.ndarray):
     out: np.ndarray = model.predict(encode(arr.flatten())).flatten()
     m = (out.min(), out.min())
-
     c=0
     for i in out:
         if i > m[1]:
-            min = (i,c)
+            m = (i,c)
         c+=1
 
     print(idx_dict[m[1]], m[0])
